7d230f056562f7f3574c7ef7c39aac32b168ebe9
anandvamsi <anandvamsi.v@gmail.com>
2023-03-30 16:44:40 +0530

    Update port.rb

--- port.rb
+++ port.rb
@@ -1,12 +1,12 @@
-control 'nginx' do
-    impact 6.5
-    title 'Check if Redis Server is using the default port 80'
-    desc 'The nginx  server must be configured such that it does not run on the default port 80. '
-    describe file('/etc/nginx/nginx.conf') do
-        its('content') { should_not match(%r{^port\ 80}) }
-        its('content') { should_not match(%r{^port\ 0}) }
-        end
-    describe port(80) do
-    it { should_not be_listening }
-    end
+ control 'redis-03' do
+    impact 6.5
+    title 'Check if Redis Server is using the default port 6379'
+    desc 'The Redis server must be configured such that it does not run on the default port 6379. '
+    describe file('/usr/local/etc/redis/redis.conf') do
+        its('content') { should_not match(%r{^port\ 6379}) }
+        its('content') { should_not match(%r{^port\ 0}) }
+        end
+    describe port(6379) do
+    it { should_not be_listening }
+    end
 end
